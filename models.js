a1a05193fa30d2776ead4155691e0c884c694b41
fremaxx <159620763+fremaxx@users.noreply.github.com>
2024-02-10 12:05:26 +0100

    Add files via upload

---
+++ models.js
@@ -0,0 +1,356 @@
+const dietModels={
+    diet: [
+        {
+            name: "Dinner Tonight: 100 Simple, Healthy Recipes for Every Night of the Week (A Defined Dish Book)",
+            image: "photos/diet1.jpg",
+            description: "Alex Snodgrass, The Comfortable Kitchen author, unveils Dinner Tonight—accessible weeknight dinners focused on cleaned up comfort food. Flexible recipes cater to various diets, ideal for the food freedom stage of health journeys. From Seared Tuna to Chipotle Turkey-Stuffed Poblano Peppers, Dinner Tonight makes delicious, healthy meals achievable for everyone, regardless of hectic schedules.",
+            price: 35,
+            amazonLink: 'https://amzn.to/4984ewJ'
+           
+        },
+        {
+            name: "The Glucose Goddess Method: The 4-Week Guide to Cutting Cravings, Getting Your Energy Back, and Feeling Amazing",
+            image: "photos/diet2.jpg",
+            description: " From the #1 bestselling author of Glucose Revolution, Jessie Inchauspé presents The Glucose Goddess Method—a four-week program for balanced blood sugar, offering 100 recipes, an interactive workbook, and science-proven strategies. Endorsed by Robert H. Lustig and David Sinclair, this guide enhances energy, curbs cravings, and promotes better sleep without calorie counting, making health accessible for all.",
+            price: 35,
+            amazonLink:'https://amzn.to/496Hm0P'
+        },
+        {
+            name: "Un-Junk Your Diet: How to Shop, Cook, and Eat to Fight Inflammation and Feel Better Forever",
+            image: "photos/diet3.jpg",
+            description: "In Un-Junk Your Diet, Desiree Nielsen, a trusted dietitian, explores anti-inflammatory eating, providing 101 food swaps and insights into overfed and undernourished issues. The book guides readers through supermarket smarts, whole foods emphasis, and debunking nutrition myths. Nielsen's engaging approach empowers healthier choices, making optimal nutrition accessible and practical",
+            price: 19.99,
+            amazonLink: 'https://amzn.to/49arWbZ'
+           
+        },
+        {
+            name: "The 40-Day Sugar Fast:Where Physical Detox Meets Spiritual Transformation Wendy Speake" ,
+            image: "photos/diet4.jpg",
+            description: "Embark on the transformative 40-Day Sugar Fast, exchanging sugar for a deeper connection with Jesus. This journey addresses emotional eating, boredom, and spiritual lethargy. Learn to shift focus from temporary comforts to Christ, discovering freedom from cravings and a renewed appetite for God's goodness. Prioritize spiritual nourishment over physical detox for soul satisfaction..",
+            price: 16.99,
+            amazonLink: 'https://amzn.to/48ba8Mv'
+        },
+        {
+            name: "Skinnytaste Simple: Easy, Healthy Recipes with 7 Ingredients or Fewer: A Cookbook",
+            image: "photos/diet5.jpg",
+            description: " Enjoy Skinnytaste Simple, a collection of 120 easy, flavorful dishes by Gina Homolka, with seven ingredients or fewer. These no-fuss recipes, from Chilaquiles to Flourless Sea Salt Brownies, offer maximum flavor and nutrition with minimal effort. Perfect for busy days, each recipe includes dietary icons and updated Weight Watchers points on the Skinnytaste site.",
+            price: 35,
+            amazonLink: 'https://amzn.to/3Uxrj7F'
+        },
+        {
+            name: "Juicing for Beginners: The Essential Guide to Juicing Recipes and Juicing for Weight Loss",
+            image: "photos/diet6.jpg",
+            description: "Juicing for Beginners is your go-to guide for weight loss and well-being. Explore detoxes, cleanses, and the art of juicing with practical tips on choosing a juicer. This book features 100 restorative recipes, highlighting the health benefits of fruits and vegetables. Discover a juicing plan that suits your lifestyle and weight loss goals, helping you feel good and get lean with every sip.",
+            price: 11.99,
+            amazonLink: 'https://amzn.to/3HWfJM6'
+        },
+        {
+            name: "Women, Food, and Hormones: A 4-Week Plan to Achieve Hormonal Balance, Lose Weight, and Feel Like Yourself Again",
+            image: "photos/diet7.jpg",
+            description: " In the New York Times bestseller Women, Food, and Hormones, Dr. Sara Gottfried unveils a female-friendly keto plan—The Gottfried Protocol. This science-based approach addresses women's unique hormonal needs, optimizing fat loss, decreasing stress, improving detoxification, and targeting belly fat. With over 50 recipes, it's a transformative guide to hormonal balance and lasting health.",
+            price: 18.99,
+            amazonLink: 'https://amzn.to/3wcgTjO'
+           
+        },
+        {
+            name: "The Hallelujah Diet:  Experience the Optimal Health You Were Always Meant to Have",
+            image: "photos/diet8.jpg",
+            description: " Based on biblical principles and extensive research, George Malkmus' The Hallelujah Diet encourages a reconsideration of daily food habits. This transformative guide explores the diet's biblical foundation, emphasizing the healing power of living and organic foods. It covers exercise, sunlight, water, stress balance, and rest for holistic health, complemented by testimonials and practical resources.",
+            price: 18.99,
+            amazonLink: 'https://amzn.to/49p6LTL'
+        },
+        {
+            name: "132+ Delicious Salads, Dressings And Dips: (Gabrielle's FUSS-FREE Healthy Veg Recipes)",
+            image: "photos/diet9.jpg",
+            description: "  In 132+ Delicious Salads, Dressings And Dips, author Gabrielle Raiz transforms basic salads into flavorful masterpieces. With over 132 recipes, delectable dressings, and creamy dips, Raiz offers creative combinations for every taste. This comprehensive cookbook, suitable for various diets, introduces innovative techniques to elevate salads into satisfying and nutritious main meals.",
+            price: 18.95,
+            amazonLink: 'https://amzn.to/49p6TTf'
+           
+        },
+        {
+            name: "The 30-Minute Mediterranean Diet Cookbook: 101 Easy, Flavorful Recipes for Lifelong Health",
+            image: "photos/diet10.jpg",
+            description: "  Explore the simplicity of the Mediterranean diet with The 30-Minute Mediterranean Diet Cookbook. Uncover its principles, essential ingredients, and potential health benefits, promoting a heart-healthy lifestyle. From avocado toast to mushroom-barley soup, the cookbook offers over 100 delicious recipes, accommodating all skill levels with beginner-friendly instructions, ensuring quick and wholesome meals for the whole family.",
+            price: 17.99,
+            amazonLink: 'https://amzn.to/4bpwI6O'
+        },
+        {
+            name: "Food List Encyclopedia for Diabetes: The Complete Updated Guide of Foods that Don’t Spike Blood Sugar to Stay Healthy",
+            image: "photos/diet11.jpg",
+            description: " Discover the power of the Mediterranean diet with The 30-Minute Mediterranean Diet Cookbook. Explore essential ingredients, guiding principles, and potential health benefits. From avocado toast to mushroom-barley soup, enjoy over 100 nourishing recipes for every meal. Beginner-friendly instructions and a variety of dishes make adopting this heart-healthy lifestyle easy and delicious.",
+            price: 17.97,
+            amazonLink: 'https://amzn.to/499UQJ0'
+           
+        },
+        {
+            name: "Eat Right 4 Your Type (Revised and Updated): The Individualized Blood Type Diet® Solution",
+            image: "photos/diet12.jpg",
+            description: " Discover personalized well-being with the New York Times bestselling Eat Right 4 Your Type. Uncover the unique diet and health plan tailored to your blood type. This revised edition offers insights into optimal foods, exercises, and lifestyle choices, empowering you to combat diseases and slow down aging without relying on restrictive diets.  ",
+            price: 30,
+            amazonLink: 'https://amzn.to/3UARURn'
+        },
+        {
+            name: "The Complete Ketogenic Diet for Beginners: Your Essential Guide to Living the Keto Lifestyle",
+            image: "photos/diet13.jpg",
+            description: "The Complete Ketogenic Diet for Beginners simplifies the start of the ketogenic lifestyle, offering meal plans, shopping lists, and support. This beginner-friendly guide explains keto fundamentals and provides 75 easy-to-follow recipes like Bacon-Artichoke Omelets. The book includes a 14-day meal plan, making keto accessible and enjoyable for weight loss or overall health. Embrace easy, delicious, and healthy keto living today.",
+            price: 11.99,
+            amazonLink: 'https://amzn.to/3HXffVX'
+        },
+        {
+            name: "Complete Guide To Fasting: Heal Your Body Through Intermittent, Alternate-Day, and Extended Fasting",
+            image: "photos/diet14.jpg",
+            description: "  Discover the ultimate guide to intermittent fasting with The Complete Guide to Fasting. Whether you're new to fasting or refining your plan, this manual offers a 7-Day Kick-Start Fasting Plan, 20 healing recipes, and answers to common questions. Learn how intermittent fasting enhances weight loss, improves health conditions, and aligns with various diets. Enjoy a simpler, effective approach to fasting!",
+            price: 35.95,
+            amazonLink: 'https://amzn.to/4890DNL'
+           
+        },
+        {
+            name: "Meal Prep for Weight Loss: Weekly Plans and Recipes to Lose Weight the Healthy Way",
+            image: "photos/diet15.jpg",
+            description: " Discover the key to long-term weight loss with Meal Prep for Weight Loss. Learn efficient meal planning and preparation to enjoy healthy, nourishing meals throughout the week. With shopping lists, recipes, and step-by-step instructions, this cookbook offers versatile plans suitable for a Mediterranean or DASH diet. Achieve sustainable weight loss success with flavorful recipes and balanced eating.  ",
+            price: 15.99,
+            amazonLink: 'https://amzn.to/3SrBKHm'
+        },
+      
+      
+    ],
+   
+    
+
+};
+
+function updatedietItems(category, container) {
+    const dietModelsList = container || document.getElementById("diet-items");
+
+    dietModelsList.innerHTML = "";
+
+    if (category === 'all') {
+        
+        const allItems = [];
+        for (const categoryKey in dietModels) {
+            if (dietModels.hasOwnProperty(categoryKey)) {
+                allItems.push(...dietModels[categoryKey]);
+            }
+        }
+
+        for (let i = 0; i < allItems.length; i += 3) {
+            const row = document.createElement("div");
+            row.classList.add("diet-row");
+
+            for (let j = i; j < i + 3 && j < allItems.length; j++) {
+                createMenuItem(allItems[j], row);
+            }
+
+            dietModelsList.appendChild(row);
+        }
+    } else {
+        
+        const items = dietModels[category];
+
+        for (let i = 0; i < items.length; i += 3) {
+            const row = document.createElement("div");
+            row.classList.add("diet-row");
+
+            for (let j = i; j < i + 3 && j < items.length; j++) {
+                createMenuItem(items[j], row);
+            }
+
+            dietModelsList.appendChild(row);
+        }
+    }
+
+
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createMenuItem(item, container) {
+    const li = document.createElement("li");
+    const name = document.createElement("span");
+    const price = document.createElement("span");
+    const addButton = document.createElement("a"); // Change to 'a' element
+    const image = document.createElement("img");
+    const description = document.createElement("p");
+
+    name.textContent = item.name;
+    price.textContent = `Price: $${item.price.toFixed(2)}`;
+
+    // Use the provided Amazon link for each button
+    addButton.href = item.amazonLink;
+    
+    addButton.textContent = "Continue";
+    addButton.setAttribute("data-name", item.name);
+    addButton.setAttribute("data-price", item.price.toFixed(2));
+
+    description.textContent = item.description;
+    image.src = item.image;
+    image.alt = item.name;
+
+    li.appendChild(name);
+    li.appendChild(image);
+    li.appendChild(description);
+    li.appendChild(price);
+    li.appendChild(addButton);
+
+    if (container) {
+        container.appendChild(li);
+    } else {
+        const row = document.createElement("div");
+        row.classList.add("diet-row");
+        row.appendChild(li);
+        dietModelsList.appendChild(row);
+    }
+}
+
+
+
+
+
+document.getElementById("all-button").click();
+
+document.addEventListener("DOMContentLoaded", function() {
+    applyPriceFiltering();
+    
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+    const dietForm = document.getElementById("diet-form");
+    dietForm.addEventListener("submit", addNewdiet);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function removeFromdiett(event){
+    event.target.parentElement.remove();
+ 
+    calculateTotal();
+ 
+   
+ }
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+ 
+
+ function updatediettItem(existingItem, itemPrice, quantity) {
+    const newPrice = itemPrice * quantity;
+    existingItem.dataset.quantity = quantity;
+    existingItem.querySelector("span:nth-child(1)").textContent = `${existingItem.dataset.name} x${quantity}`;
+    existingItem.querySelector("span:nth-child(2)").textContent = `$${newPrice.toFixed(2)}`;
+
+    calculateTotal(); 
+}
+
+function calculateTotal(){
+    const basketItems = document.querySelectorAll("#basket-items li");
+    let subtotal = 0;
+    let tax = 0;
+    let total = 0;
+    const taxRate= 0.18;
+
+    basketItems.forEach((item) =>{
+        const itemPrice = parseFloat(item.querySelector("span:nth-child(2)").textContent.slice(1));
+        subtotal += itemPrice;
+    });
+
+    tax = subtotal * taxRate;
+    total = subtotal + tax;
+
+    document.querySelector("#subtotal-price").textContent = `${subtotal.toFixed(2)}`;
+    document.querySelector("#tax-price").textContent = `${tax.toFixed(2)}`;
+    document.querySelector("#totali-price").textContent = `${total.toFixed(2)}`;
+}
+
+
+
+const diet = document.getElementById("diet");
+diet.addEventListener("change" , updatedietItems);
+
+updatedietItems();
+
+
+
+
+const diettList = document.getElementById("basket-items");
+
+diettList.addEventListener("click", (event) => {
+if (event.target.classList.contains("add") || event.target.classList.contains("remove")){
+calculateTotal();
+}
+});
+
+calculateTotal();
+
+
